b564fb78a2345a3b61d37aae5ae70a57547ccc61
Harish <164208393+hnagiset@users.noreply.github.com>
2024-04-04 14:35:55 +0530

    Enhance the basic test

--- tests/basic-test.sv
+++ tests/basic-test.sv
@@ -1,4 +1,55 @@
-module hello();
+   /*
+The quick brown fox
+  module
+*/
+  // basic-test.sv
+// hello
+   module hello();
 abc;
 def;
+   module hello();
+abc;
+     if (abc)
+       if (abc)
+def;
+       else
+         begin
+         ghi;
+         ghi;
+  `ifdef hi
+           if abc
+             `endif
+             afunction(a,
+                   b,
+                   c);
+             bfunction(
+a,
+             cfunction(
+  a,
+  a,
+  a,
+  a)
+                   b, c);
+           while (a)
+xyz;
+         ghi;
+         ghi;
+           end
+while (a) begin
+ abc;
+end
+
+     fork;
+       ab;
+     join
+
+     wait fork;
+      ab;
+xyz;
+def;
+   module hello();
+abc;
+def;
+endmodule
+endmodule
 endmodule
